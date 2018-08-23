18cd3bc5c1b1cb2c0a288736731021008bedae54
Andreia Trianon <andreiatrianon@outlook.com>
2018-08-23 16:49:02 -0300

    solução parcial feita em aula

--- formatter.js
+++ formatter.js
@@ -1,3 +1,12 @@
 module.exports.valueToAPIFormat = function findURLsInText(number) {
 
+  if (!number) {
+    return '0';
+  }
+
+  const numberWithDot = number.replace(',', '.');
+  const numeroDecimal = Number.parseFloat(numberWithDot).toFixed(2);
+
+  return numeroDecimal;
+
 };
--- url.spec.js
+++ url.spec.js
@@ -1,19 +1,25 @@
-var mocha = require("mocha");
-var chai = require("chai");
-var url = require("./url");
-var expect = chai.expect;
+const mocha = require("mocha");
+const chai = require("chai");
+const url = require("./url");
+const expect = chai.expect;
 
 describe("url", function() {
   describe("#findURLsInText", function() {
     describe("when there is no url in the text", function() {
       it("should return an empty array", function() {
 
+        expect(url.findURLsInText("oi tudo bem?")).to.be.an('array');
+        expect(url.findURLsInText("oi tudo bem?")).to.be.empty;
+
       });
     })
 
     describe("when there is one url in the text", function() {
+
       describe("and it has HTTPS", function() {
         it("should return the url in an array", function() {
+
+          expect(url.findURLsInText("oi https://www.google.com tudo bem?")).to.deep.equal(['https://www.google.com']);;
 
         });
       });
@@ -21,17 +27,23 @@ describe("url", function() {
       describe("and it has HTTP", function() {
         it("should return the url in an array", function() {
 
+          expect(url.findURLsInText("oi http://www.google.com tudo bem?")).to.deep.equal(['http://www.google.com']);;
+       
         });
       });
 
       describe("and it has www", function() {
         it("should return the url in an array", function() {
 
+          expect(url.findURLsInText("oi www.google.com tudo bem?")).to.deep.equal(['www.google.com']);;
+        
         });
       });
 
       describe("and it only the domain", function() {
         it("should return the url in an array", function() {
+
+          expect(url.findURLsInText("oi google.com tudo bem?")).to.deep.equal(['google.com']);
 
         });
       });
@@ -40,6 +52,8 @@ describe("url", function() {
     describe("when there are three urls in the text", function() {
       it("should return the urls in an array", function() {
 
+        expect(url.findURLsInText("oi google.com.br www.laboratoria.la https://ibmec.com.br tudo bem?")).to.deep.equal(['google.com.br', 'www.laboratoria.la', 'https://ibmec.com.br']);
+        
       });
     })
   });
